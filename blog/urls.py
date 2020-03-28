4ffa14f6879973562b7af75acafaafcf87d6e3c9
C3f3 <armandoher01@gmail.com>
2020-03-28 01:25:24 -0300

    vistas agregadas, falta plantilla

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     #la cadena debe coincidir con inicio y final
     #la unica cadena que va a coincidir es vacio
+    #post_list es lan funcion que renderiza la plantilla html
     url(r'^$',views.post_list),
 ]
